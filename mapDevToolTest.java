107fc71191cb4a541c61012882a1ce931c7b8a67
Michael Bradford <esspecialmkb@gmail.com>
2016-04-18 19:39:51 -0400

    Update mapDevToolTest.java
    Completed functionality to change block target

--- mapDevToolTest.java
+++ mapDevToolTest.java
@@ -1,9 +1,9 @@
 /** 
  * 
- *  Map Tool Dev r1
+ *  Map Tool Dev r2
  *  This code resues the TestPicking example from cubes engine
  *  Voxel size and camera movement speed is now adjusted to comfortable (useable) level
- *  TODO: Add ability to change target block type to add
+ *  Done: Add ability to change target block type to add
  *  TODO: Load/Save ability (currently being developed in fileMAnagementDev.java)
  *  TODO: Create block type to register game node (i.e. spawn points, item placement, interactive objects/blocks)
  */
@@ -25,6 +25,8 @@ import com.jme3.system.AppSettings;
 import com.jme3.scene.Node;
 import com.cubes.*;
 import com.cubes.test.CubesTestAssets;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.KeyTrigger;
 
 public class mapDevToolTest extends SimpleApplication implements ActionListener{
 
@@ -42,6 +44,23 @@ public class mapDevToolTest extends SimpleApplication implements ActionListener{
     }
     private Node terrainNode;
     private BlockTerrainControl blockTerrain;
+    
+    /**
+     * 
+     *  The currentBlock data member will keep track of the currently used block
+     *  0 - BLOCK_GRASS
+     *  1 - BLOCK_WOOD
+     *  2 - BLOCK_WOOD_FLAT
+     *  3 - BLOCK_BRICK
+     *  4 - BLOCK_CONNECTOR_ROD
+     *  5 - BLOCK_GLASS
+     *  6 - BLOCK_STONE
+     *  7 - BLOCK_STONE_PILLAR
+     *  8 - BLOCK_WATER
+     */
+    private int currentBlock;
+    // This bitmap font will remind the user of the choosen block
+    private BitmapText currentBlockDisplay;
 
     @Override
     public void simpleInitApp(){
@@ -52,13 +71,55 @@ public class mapDevToolTest extends SimpleApplication implements ActionListener{
         cam.setLocation(new Vector3f(-16.6f, 46, 97.6f));
         cam.lookAtDirection(new Vector3f(0.68f, -0.47f, -0.56f), Vector3f.UNIT_Y);
         flyCam.setMoveSpeed(15);
+        currentBlock = 0;
     }
+    
+    // Callback-helper method to update currentBlock data from onAction method
+    private void changeCurrentBlock() {
+        currentBlock = currentBlock + 1;
+        if(currentBlock > 8) {
+            currentBlock = 0;
+        }
+        switch(currentBlock) {
+            case 0:
+                currentBlockDisplay.setText("Current Block: 0 - BLOCK_GRASS");
+                break;
+            case 1:
+                currentBlockDisplay.setText("Current Block: 1 - BLOCK_WOOD");
+                break;
+            case 2:
+                currentBlockDisplay.setText("Current Block: 2 - BLOCK_WOOD_FLAT");
+                break;
+            case 3:
+                currentBlockDisplay.setText("Current Block: 3 - BLOCK_BRICK");
+                break;
+            case 4:
+                currentBlockDisplay.setText("Current Block: 4 - BLOCK_CONNECTOR_ROD");
+                break;
+            case 5:
+                currentBlockDisplay.setText("Current Block: 5 - BLOCK_GLASS");
+                break;
+            case 6:
+                currentBlockDisplay.setText("Current Block: 6 - BLOCK_STONE");
+                break;
+            case 7:
+                currentBlockDisplay.setText("Current Block: 7 - BLOCK_STONE_PILLAR");
+                break;
+            case 8:
+                currentBlockDisplay.setText("Current Block: 8 - BLOCK_WATER");
+                break;
+        }
+    } 
     
     private void initControls(){
         inputManager.addMapping("set_block", new MouseButtonTrigger(MouseInput.BUTTON_LEFT));
         inputManager.addListener(this, "set_block");
         inputManager.addMapping("remove_block", new MouseButtonTrigger(MouseInput.BUTTON_RIGHT));
         inputManager.addListener(this, "remove_block");
+        
+        // Adding mapping and listener for change_block action
+        inputManager.addMapping("change_block", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(this, "change_block");
     }
     
     // initBlockTerrain() encapsulates the process of creating the voxel map
@@ -76,6 +137,7 @@ public class mapDevToolTest extends SimpleApplication implements ActionListener{
     }
     
     private void initGUI(){
+        
         //Crosshair
         BitmapText crosshair = new BitmapText(guiFont);
         crosshair.setText("+");
@@ -97,6 +159,12 @@ public class mapDevToolTest extends SimpleApplication implements ActionListener{
         instructionsText3.setText("(Bottom layer is marked as indestructible)");
         instructionsText3.setLocalTranslation(0, settings.getHeight() - (2 * instructionsText3.getLineHeight()), 0);
         guiNode.attachChild(instructionsText3);
+        
+        //CurrentBlock indicator
+        currentBlockDisplay = new BitmapText(guiFont);
+        currentBlockDisplay.setText("Current Block: 0 - BLOCK_GRASS");
+        currentBlockDisplay.setLocalTranslation(0, settings.getHeight() - (3 * instructionsText3.getLineHeight()), 0);
+        guiNode.attachChild(currentBlockDisplay);
    }
 
     @Override
@@ -104,7 +172,37 @@ public class mapDevToolTest extends SimpleApplication implements ActionListener{
         if(action.equals("set_block") && value){
             Vector3Int blockLocation = getCurrentPointedBlockLocation(true);
             if(blockLocation != null){
-                blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_WOOD);
+                //blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_WOOD);
+                //The currently selected block determines what type of block to add
+                switch(currentBlock) {
+                    case 0:
+                        blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_GRASS);
+                        break;
+                    case 1:
+                        blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_WOOD);
+                        break;
+                    case 2:
+                        blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_WOOD_FLAT);
+                        break;
+                    case 3:
+                        blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_BRICK);
+                        break;
+                    case 4:
+                        blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_CONNECTOR_ROD);
+                        break;
+                    case 5:
+                        blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_GLASS);
+                        break;
+                    case 6:
+                        blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_STONE);
+                        break;
+                    case 7:
+                        blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_STONE_PILLAR);
+                        break;
+                    case 8:
+                        blockTerrain.setBlock(blockLocation, CubesTestAssets.BLOCK_WATER);
+                        break;
+                }
             }
         }
         else if(action.equals("remove_block") && value){
@@ -113,6 +211,10 @@ public class mapDevToolTest extends SimpleApplication implements ActionListener{
             if((blockLocation != null) && (blockLocation.getY() > 0)){
                 blockTerrain.removeBlock(blockLocation);
             }
+        }
+        else if(action.equals("change_block") && value) {
+            // Call the helper method: changeCurrentBlock
+            changeCurrentBlock();
         }
     }
     
@@ -135,4 +237,3 @@ public class mapDevToolTest extends SimpleApplication implements ActionListener{
         return results;
     }
 }
-
